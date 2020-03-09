fadbce658a698898a21e3ec01b8ccdab1ee91e26
ninggc <906978985@qq.com>
2020-03-09 21:53:38 +0800

    update

---
+++ service-hi/src/test/java/cn/ninggc/leanrning/servicehi/ServiceHiApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.ninggc.leanrning.servicehi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHiApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
---
+++ service-miya/src/test/java/cn/ninggc/learning/servicemiya/ServiceMiyaApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.ninggc.learning.servicemiya;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceMiyaApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
